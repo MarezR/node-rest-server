0302d251743b10d86c90b089d7213521c88c90cb
Juan Roberto Rivero Mares <jriveroma@guanajuato.gob.mx>
2021-01-29 20:24:09 -0600

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,51 @@
+require('./config/config');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', (req, res) => {
+    res.json("get Usuario");
+});
+
+app.post('/usuario', (req, res) => {
+
+
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        });
+    } else {
+
+        res.json({ persona: body });
+    }
+
+
+});
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json("delete Usuario");
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando el puerto 3000");
+})
